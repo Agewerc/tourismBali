75fa564a565a9e2983f72c88b194885f1a935cd2
Alan Gewerc <agew0001@student.monash.edu>
2020-11-09 16:45:16 +1100

    changes in UI - navbar and tabs

--- generate_name.R
+++ generate_name.R
@@ -2,6 +2,7 @@
 
 # Load the package required to read JSON files.
 library("rjson")
+library('stringr')
 
 # Give the input file name to the function.
 result <- fromJSON(file = "treated_bali_data.json")
@@ -22,9 +23,14 @@ for (i in 1:length(result)){
 
 }
 
-vector_label <- unique(vector_labels)
+vector_tags <- str_remove(vector_tags, pattern = "foursquare.*")
+vector_tags <- vector_tags[vector_tags != ""]
+
+
+vector_labels <- unique(vector_labels)
 vector_tags <- unique(vector_tags)
 vector_countys <- unique(vector_countys)
+
 
 saveRDS(vector_labels, file = "labels.Rds")
 saveRDS(vector_tags, file = "tags.Rds")
--- ui.R
+++ ui.R
@@ -9,38 +9,64 @@
 
 library(shiny)
 
-
+length(label_vector)
 label_vector <- readRDS("labels.rds")
 tag_vector <- readRDS("tags.rds")
 county_vector <- readRDS("countys.rds")
-
+image_list <- c('5852495442.jpg', '15550188123.jpg', '15582001714.jpg', '15581992784.jpg')
 
 
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-    
-    sidebarPanel(),
-    
-    mainPanel(
 
-    # Application title
-    titlePanel("Bali Tourism"),
+    
+    navbarPage("Bali Tourism",
 
-    # Sidebar with a slider input for number of bins
+               tabPanel("Places and Photos",
+                        
+
+                        ########################################################
+                        ################### input labels, tags and county
+                        ########################################################
+                        
+                        
+                        fluidRow(
+                            column(2, selectInput("labels",label="Choose Label",multiple = TRUE, 
+                                                                         choices=label_vector)), 
+                                   
+
+                            column(2, selectInput("tags",label="Choose Tag",multiple = TRUE, 
+                                                  choices=tag_vector)), 
+                            
+                            
+                            column(2, selectInput("countys",label="Choose County",multiple = TRUE, 
+                                                  choices=county_vector))
+                            ),
+                        
+                        tabsetPanel(type = "tabs",
+                                    
+                                    tabPanel("Map"),
+
+                           
+                                    tabPanel("Images",
+                                             
+                                             lapply(image_list, FUN = function(i) {
+                                                 
+                                                 img(src = paste0(i), height = 400, width = 460) }
+                                             )
+                                             )
+                                    )),
+
+               tabPanel("Regions"),
+               tabPanel("Tourits Origin"),
+               tabPanel("About")
     
-    selectInput(
-        inputId = 'country_compare',
-        label = 'Select One or More Labels',
-        choices = label_vector,
-        multiple = TRUE,
-        selected = c('United States', 'Brazil', 'India')
-        ),
-    
-    
-    img(src = "https://media.timeout.com/images/105240189/image.jpg", height = 72, width = 72)
-    
+
+               
     )
-
-        )
     )
+)
+
+
+
