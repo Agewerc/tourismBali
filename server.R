0df19e2df4c6cd75c42aba3822173a403015bee0
Alan Gewerc <agew0001@student.monash.edu>
2020-11-11 15:04:10 +1100

    Adding data CSV for wordcloud

--- server.R
+++ server.R
@@ -12,14 +12,65 @@ library(gridExtra)
 library(png)
 library(grid)
 library(leaflet)
-
+library(geojsonio)
+library(sf)
 
 bali_data <- fromJSON(file = "data_bali.json")
+
 
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
+    bali_data <- fromJSON(file = "data_bali.json")
+    states <- geojsonio::geojson_read("bali.geojson", what = "sp")
+    bins <- c(0, 500, 1000, 1500, 2000, 2500, 3000, Inf)
+    pal <- colorBin("YlGnBu", domain = states$density, bins = bins)
+    
+    output$baliclo <- renderLeaflet({
+        
+        labels <- sprintf(
+            "<strong>%s</strong><br/>%g Images",
+            states$nm_kabkota, states$density
+        ) %>% lapply(htmltools::HTML)
+        
+
+        m <- leaflet(states) %>%
+            setView(lat = -8.48, lng = 115.1, zoom = 10) %>%
+            addTiles %>% 
+            addPolygons(
+                fillColor = ~pal(density),
+                weight = 2,
+                opacity = 1,
+                color = "white",
+                dashArray = "3",
+                fillOpacity = 0.7,
+                highlight = highlightOptions(
+                    weight = 5,
+                    color = "#666",
+                    dashArray = "",
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE),
+                label = labels,
+                labelOptions = labelOptions(
+                    style = list("font-weight" = "normal", padding = "3px 8px"),
+                    textsize = "15px",
+                    direction = "auto")) %>%
+            addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
+                      position = "bottomleft") %>%
+            addProviderTiles(providers$Esri.NatGeoWorldMap)
+
+        })
+    
+    
+    observe({ 
+        event <- input$baliclo_shape_click
+        output$cnty <- renderText(shape$NAME[shape$CNTY_ID == event$id])
+        
+    })
+    
+        
+        
   
   output$img2 = renderUI({
     
@@ -154,8 +205,13 @@ shinyServer(function(input, output) {
                              color = "#03F",
                              lat = ~lat,
                              radius = 1,
-                             popup = paste0("<img src = ", sup_vector, ".jpg>")
+                             popup = paste0("<img src = ", sup_vector, ".jpg width=300 height=300>")
         )
+      }else {
+          
+          leaflet() %>%
+              addTiles() %>%
+              setView(lat = -8.48, lng = 115.1, zoom = 10)
       }
                          
                          # dashArray = NULL,
